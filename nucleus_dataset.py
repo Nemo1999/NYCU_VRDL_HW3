e799b8c5d9a777b495aa89c3746d74bce95d6d9a
BO YU, Cheng <nemo1999.eecs06@g2.nctu.edu.tw>
2021-12-14 01:05:30 +0800

    Evaluate and output COCO format

--- nucleus_dataset.py
+++ nucleus_dataset.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import json
 
 def nucleus_dataset(train_set_path="dataset/train", mode="train"):
     """
@@ -22,8 +23,26 @@ def nucleus_dataset(train_set_path="dataset/train", mode="train"):
         train_set.append(img_dict)
     if mode == "train":
         return train_set[:int(len(train_set) * 0.8)]
-    else:
+    elif mode == "validate":
         return train_set[int(len(train_set) * 0.8):]
+    else:
+        assert mode=="test", "mode must be either train, validate or test"
+        with open("dataset/test_img_ids.json", "r") as f:
+            test_json = json.load(f)
+        test_set = []        
+        for test_img in test_json:
+            file_name = os.path.join(train_set_path.replace("train", "test"), test_img["file_name"])
+            height , width = test_img["height"], test_img["width"]
+            image_id = test_img["id"]
+            img_dict = {
+                "file_name": file_name,
+                "height": height,
+                "width": width,
+                "image_id": image_id
+            }
+            test_set.append(img_dict)   
+        return test_set
+            
 
 # test code
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,22 +1,33 @@
 import random
+import numpy as np
 import os
 import cv2
+import argparse
+import json
+from datetime import datetime
+from pycocotools.mask import encode, toBbox
+from nucleus_dataset import nucleus_dataset
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.utils.visualizer import Visualizer
-from nucleus_dataset import nucleus_dataset
-from detectron2.engine import DefaultTrainer
+from detectron2.utils.visualizer import ColorMode
+from detectron2.engine import DefaultTrainer, DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
 from detectron2.config import LazyConfig
+from tqdm import tqdm
 
 DatasetCatalog.register("nucleus_train", lambda : nucleus_dataset(mode="train"))
 MetadataCatalog.get("nucleus_train").set(thing_classes=["nucleus"]) # set the thing_classes
 DatasetCatalog.register("nucleus_val", lambda : nucleus_dataset(mode="validation"))
 MetadataCatalog.get("nucleus_val").set(thing_classes=["nucleus"]) # set the thing_classes
+DatasetCatalog.register("nucleus_test", lambda : nucleus_dataset(mode="test"))
+
 nucleus_metadata = MetadataCatalog.get("nucleus_train")
 nucleus_metadata = MetadataCatalog.get("nucleus_val")
+
 """
-Visualize Annotation (Check loaded correctly)
+
+#Visualize Annotation (Check loaded correctly)
 
 dataset_dicts = nucleus_dataset()
 for d in random.sample(dataset_dicts, 3):
@@ -28,28 +39,97 @@ for d in random.sample(dataset_dicts, 3):
     cv2.waitKey(0)
 """
 
+parser = argparse.ArgumentParser(description="Detectron2 Training")
+parser.add_argument("--resume", default=False, action="store_true", help="Resume from the model")
+parser.add_argument("--outdir", type=str, default="default", help="Output directory")
+parser.add_argument("--eval", default=False, action="store_true", help="Evaluate the model")
+parser.add_argument("--model_path", type=str, default="", help="Path to the model")
+parser.add_argument("--config_path", type=str, default="", help="Path to the config")
+parser.add_argument("--visulize", default=False, action="store_true", help="Visualize Evaluation Results")
 
-
+cmd_args = parser.parse_args()
 
 cfg = get_cfg()
+if not cmd_args.resume and not cmd_args.eval:
+    if cmd_args.outdir == "default":
+        cfg.OUTPUT_DIR = f'output/{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+    else:
+        cfg.OUTPUT_DIR = cmd_args.outdir
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+else:
+    cfg.OUTPUT_DIR = cmd_args.outdir
+
 #cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_regnetx_4gf_dds_fpn_1x.py"))
-cfg.OUTPUT_DIR = "./output"
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 cfg.DATASETS.TRAIN = ("nucleus_train",)
-cfg.DATASETS.TEST = ("nuclues_val",)
+cfg.DATASETS.TEST = ("nucleus_val",)
 cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = "Pretrained/model_final_f1362d.pkl"
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+#cfg.MODEL.WEIGHTS = "Pretrained/model_final_f1362d.pkl"
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.00025
-cfg.SOLVER.MAX_ITER = 3000
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
+cfg.SOLVER.MAX_ITER = 10000
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 625
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
 cfg.INPUT.MASK_FORMAT = "bitmask"  # alternative: "polygon"
+
+
 #cfg = LazyConfig.load(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_regnetx_4gf_dds_fpn_1x.py"))
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg)
-trainer.resume_or_load(resume=False)
-trainer.train()
+if cmd_args.eval == False:
+    trainer = DefaultTrainer(cfg)
+    trainer.resume_or_load(resume=cmd_args.resume)
+    trainer.train()
+else:
+    cfg.MODEL.WEIGHTS = os.path.join(cmd_args.model_path)  # path to the model we just trained
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.01   # set a custom testing threshold
+    predictor = DefaultPredictor(cfg)
+    # Load and display evaluation results
+    dataset_dicts = nucleus_dataset(mode="train")
+    if cmd_args.visulize:
+        for d in dataset_dicts[:3]:    
+            img = cv2.imread(d["file_name"])
+            outputs = predictor(img)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+            
+            v = Visualizer(img[:, :, ::-1],
+                    metadata=nucleus_metadata, 
+                    scale=1.0, 
+                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+            )
+
+            visualizer = Visualizer(img[:, :, ::-1], metadata=nucleus_metadata, scale=1.0 )
+            vis = visualizer.draw_dataset_dict(d)
+            gt_img = vis.get_image()[:, :, ::-1]
+            out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+            out_img = out.get_image()[:, :, ::-1]
+            cv2.imshow("Eval_Result",np.concatenate((gt_img, out_img), axis=1))
+            cv2.waitKey(0)
+        exit(0)
+    else:
+        # output  COCO style evaluation results
+        """
+        [{
+            "image_id": int, 
+            "category_id": int, 
+            "segmentation": RLE, 
+            "score": float,
+        }]
+        """
+        coco_result = []
+        dataset_dicts = nucleus_dataset(mode="test")
+        for d in dataset_dicts:
+            img = cv2.imread(d["file_name"])
+            outputs = predictor(img)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+
+            image_id = d["image_id"]
+            category_id = 1
+            for i in tqdm(range(outputs["instances"].scores.shape[0])):
+                segmentation = encode(np.array(outputs["instances"].pred_masks[i].cpu(), order="F"))
+                segmentation["counts"] = str(segmentation["counts"],'utf-8')
+                score = outputs["instances"].scores[i].item()
+                coco_result.append({"image_id": image_id, "category_id": category_id, "segmentation": segmentation, "score": score})
+        with open(os.path.join(cfg.OUTPUT_DIR, "answer.json"), "w") as f:
+            json.dump(coco_result, f)
 
 
 
